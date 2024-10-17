96bf276205fcffc74f6491e7e5884621f1219b24
Pixitt <colinjcooley@protonmail.com>
2024-10-17 03:46:06 -0500

    Init commit

---
+++ src/main/java/net/pixit/illuminate/IlluminateDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pixit.illuminate;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IlluminateDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
